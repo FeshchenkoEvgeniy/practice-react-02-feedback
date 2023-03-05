561f85e27e7560f545b4ea4ee5c990a3de7017b5
Evgeniy <feshchenko.evgeniy03@gmail.com>
2023-03-05 21:28:54 +0300

    update

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,11 @@
 import React, { Component } from 'react';
+import { FeedbackOptions } from './FeedbackOptions/FeedbackOptions';
+import { Section } from './Section/Section';
+import { Statistics } from './Stats/Statistics ';
+import { Notification } from './Notification/Notification';
+import { Container } from './App.styled';
+
+const options = ['good', 'neutral', 'bad'];
 
 class App extends Component {
   state = {
@@ -6,52 +13,49 @@ class App extends Component {
     neutral: 0,
     bad: 0,
   };
-  postiveFeedbackIncrement = () => {
-    this.setState({ good: this.state.good + 1 });
+
+  handleFeedback = evt => {
+    const elem = evt.target.innerText.toLowerCase();
+    this.setState(prevState => ({
+      [elem]: prevState[elem] + 1,
+    }));
   };
 
-  neutralFeedbackIncrement = () => {
-    this.setState({ neutral: this.state.neutral + 1 });
-  };
-
-  badFeedbackIncrement = () => {
-    this.setState({ bad: this.state.bad + 1 });
-  };
-    
   countTotalFeedback = () => {
-    let total = this.state.good + this.state.neutral + this.state.bad;
-    return total;
-  }
+    const { good, neutral, bad } = this.state;
+    return good + neutral + bad;
+  };
+
   countPositiveFeedbackPercentage = () => {
-    let feedback = this.state.good / (this.state.good + this.state.bad + this.state.neutral) * 100;
-    let abc = Math.round(feedback)
-    return abc;
-  }
-  render() {
+    const { good } = this.state;
     let total = this.countTotalFeedback();
-    let feedback = this.countPositiveFeedbackPercentage();
-    console.log(isNaN(feedback))
+    return total ? Math.round((good / total) * 100) : 0;
+  };
+
+  render() {
+    let totalFeedbackCount = this.countTotalFeedback();
+    let positiveFeedback = this.countPositiveFeedbackPercentage();
     return (
-      <div>
-        <h1>Відгуки</h1>
-        <button type="button" onClick={this.postiveFeedbackIncrement}>
-          Good
-        </button>
-        <button type="button" onClick={this.neutralFeedbackIncrement}>
-          Neutral
-        </button>
-        <button type="button" onClick={this.badFeedbackIncrement}>
-          Bad
-        </button>
-        <div>
-          <h2>Stats</h2>
-          <p>Good: {this.state.good}</p>
-          <p>Neutral: {this.state.neutral}</p>
-          <p>Bad: {this.state.bad}</p>
-          <p>Total: {total}</p>
-          <p>Positive feedback: {isNaN(feedback) ? 0 : feedback}%</p>
-        </div>
-      </div>
+      <Container>
+        <Section title={'Please leave feedback'} />
+        <FeedbackOptions
+          options={options}
+          onLeaveFeedback={this.handleFeedback}
+        />
+        <Section title={'Statistics'}>
+        {totalFeedbackCount ? (
+          <Statistics
+            good={this.state.good}
+            neutral={this.state.neutral}
+            bad={this.state.bad}
+            total={totalFeedbackCount}
+            positivePercentage={positiveFeedback}
+          />
+        ) : (
+          <Notification message="There is no feedback" />
+        )}
+        </Section>
+      </Container>
     );
   }
 }
